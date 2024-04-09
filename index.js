4f15cefb811741b6410ef08ebd4237da5383304c
Gilia Góngora <giliagongora.g@gmail.com>
2024-04-09 17:25:53 -0400

    Imprimiendo elementos

--- index.js
+++ index.js
@@ -9,17 +9,13 @@ app.get("/saludo", (req, res) => {
   res.send("¡Hola, mundo!");
 });
 
-// Definir la ruta de los archivos estáticos (para imágenes)
-app.use(express.static(__dirname + '/img'));
-
 // Definir Handlebars como motor de plantillas
 app.set("view engine", "handlebars");
 
 // Configuración del motor de plantillas
 app.engine(
-  // Define la extensión que usaremos
-  "handlebars",
-  exphbs.engine({
+    // Define la extensión que usaremos
+  "handlebars", exphbs.engine({
     // Extensiones de los archivos de plantillas
     extname: ".handlebars",
     // Plantilla principal
@@ -31,50 +27,58 @@ app.engine(
   })
 );
 
-// Creación de ruta raíz del servidor, por defecto buscará main.handlebars
-app.get("/", (req, res) => {
-  res.render("main");
-});
-
 // Definición de rutas estáticas para CSS y JavaScript de Bootstrap y jQuery
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
 app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist"));
+// Definir la ruta de los archivos estáticos (para imágenes)
+app.use(express.static(__dirname + '/img'));
+
+// Creación de ruta raíz del servidor, por defecto buscará main.handlebars
+// app.get("/", function (req, res)  {
+//   res.render("main");
+// });
 
 // Arreglo para iterar 
 // Endpoint para renderizar la vista que contiene el partial
-app.get('/data', (req, res) => {
+app.get('/', function (req, res)  {
   // en el metodo render como argumentos el main y el objeto cardData 
   res.render('main', {
-    data: [
-    {
-      nombre: 'Banana',
-      imagen: '/banana.png',
-      descripcion: 'Informacion sobre logo creado 1980'
-    },{
-      nombre: 'Cebollas',
-      imagen: '/cebollas.png',
-      descripcion: 'Informacion sobre logo creado 1980'
-    },{
-      nombre: 'Pimentón',
-      imagen: '/pimenton.png',
-      descripcion: 'Informacion sobre logo creado 1980'
-    },{
-      nombre: 'Papas',
-      imagen: '/papas.png',
-      descripcion: 'Informacion sobre logo creado 1980'
-    },{
-      nombre: 'Lechuga',
-      imagen: '/lechuga.png',
-      descripcion: 'Informacion sobre logo creado 1980'
-    },{
-      nombre: 'Tomate',
-      imagen: '/tomate.png',
-      descripcion: 'Informacion sobre logo creado 1980'
-    }
-  ]
+    layout: 'main',
+    producto: ["Banana", "Cebollas", "Lechuga", "Papas", "Pimenton", "Tomate"]
+  //   productos: [
+  //   {
+  //     nombre: 'Banana',
+  //     img: '/banana.png',
+  //     descripcion: 'Banana'
+  //   },{
+  //     nombre: 'Cebollas',
+  //     img: '/cebollas.png',
+  //     descripcion: 'Cebollas'
+  //   },{
+  //     nombre: 'Pimentón',
+  //     img: '/pimenton.png',
+  //     descripcion: 'Pimentón'
+  //   },{
+  //     nombre: 'Papas',
+  //     imagen: '/papas.png',
+  //     descripcion: 'Papas'
+  //   },{
+  //     nombre: 'Lechuga',
+  //     img: '/lechuga.png',
+  //     descripcion: 'Lechuga'
+  //   },{
+  //     nombre: 'Tomate',
+  //     img: '/tomate.png',
+  //     descripcion: 'Tomate'
+  //   }
+  // ]
+ 
 });
+
 });
+
+
 
 // Iniciar el servidor
 const PORT = 3000;
